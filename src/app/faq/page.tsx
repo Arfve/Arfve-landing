017bdeb198ca1bfd6e6ebb426099e3c6670166d4
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2025-02-18 18:28:30 +0100

    fixade funktion som renderar frågorna och svaren

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,13 +1,32 @@
 import React from "react";
-import Question from "@/components/FAQ";
+import Questions from "@/components/FAQ";
+import { getFaqPageData } from "@/lib/getFaqPageData";
 
-const page = () => {
+interface FaqPageData {
+    question: string;
+    answer: string;
+}
+
+const page = async () => {
+
+
+
+    const faqPageData: FaqPageData[] = await getFaqPageData() || [];
+
+    
+    const renderFaqContent = () => {
+        return faqPageData.map((faq, index) => {
+            return (
+                <Questions key={index} answer={faq.answer}>{faq.question}</Questions>
+            )
+        })
+    }
+
   return (
       
     <main className="m-0 flex items-center min-w-xs min-h-[72.5vh] bg-white" >
         <div className="max-w-7xl mx-auto p-8 text-center">
-            <Question answer="Lorem ipsum">Are earphones suitables for sport activities ?</Question>
-            <Question answer="Lorem ipsum">Is there some money back policy ?</Question>
+           {renderFaqContent()}
         </div>
     </main>
 )
