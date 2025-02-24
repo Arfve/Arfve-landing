d224bc4a4abb515b5380ff4bbacab46783fbbae7
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2025-02-24 11:07:47 +0100

    fixade vh

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -34,9 +34,9 @@ const page = async () => {
     }
 
     return (
-        <main className="m-0 flex items-center min-w-xs min-h-[72.5vh] bg-white" >
+        <main className="m-0 flex items-center min-w-xs min-h-[69.2vh] bg-white" >
             <div className="max-w-7xl mx-auto p-8 text-center">
-                <h1 className="text-2xl text-black font-semibold mb-6">{faqPageData.title}</h1>
+                <h1 className="text-2xl text-black font-semibold mb-6 text-left">{faqPageData.title}</h1>
                 {renderFaqContent()}
             </div>
         </main>
