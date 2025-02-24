1d8c0423efbbf144e75b7f46ce2ddc7e8fe62b47
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2025-02-24 20:13:59 +0100

    update for Crowdfunding: added the component for all of the three,

---
+++ src/components/crowdfunding/industryEndorsements.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndustryEndorsements: React.FC = (industryEndorsements) => {
+    return (
+        <div>
+            <h2>Industry Endorsements</h2>
+            <p>This section will showcase endorsements from industry leaders.</p>
+        </div>
+    );
+};
+
+export default IndustryEndorsements;
