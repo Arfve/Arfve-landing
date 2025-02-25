5990c198a2d38c1587bb57176a799c720994f157
1saifu <saifukhan@hotmail.se>
2025-02-25 13:01:43 +0100

    Fixing repsonsive

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -11,10 +11,9 @@ interface HeroProps {
 
 export default function Hero({ title, subtitle, buttonText, image }: HeroProps) {
   return (
-    <section className="relative min-h-[500px] md:h-[675px] bg-[#dedede] px-6 py-12 md:p-0 ">
-      <div className="max-w-[1440px] h-[675px] mx-auto flex items-center justify-center">
+<section className="relative min-h-[500px] md:h-[675px] bg-[#dedede] px-6 py-12 md:p-0 overflow-hidden">      <div className="max-w-[1440px] h-full mx-auto flex flex-col md:flex-row items-center justify-center text-center md:text-left"> {/* Changed flex settings */}
         {image && (
-          <div className="relative w-1/2 aspect-[5/4]">
+          <div className="relative w-full md:w-1/2 aspect-[5/4] mb-8 md:mb-0"> {/* Adjusted width and margin */}
             <Image 
               src={image}
               alt={title || "Arfve Headphones"}
@@ -26,9 +25,9 @@ export default function Hero({ title, subtitle, buttonText, image }: HeroProps) 
           </div>
         )}
 
-        <div className="flex flex-col items-center justify-center w-1/2 px-12">
-          <h1 className="text-6xl mb-4 text-black text-center font-bold">{title}</h1>
-          <p className="text-xl my-4 text-black text-center">{subtitle}</p>
+        <div className="flex flex-col items-center justify-center w-full md:w-1/2 px-12"> {/* Adjusted width */}
+          <h1 className="text-4xl md:text-6xl mb-4 text-black font-bold">{title}</h1> {/* Adjusted font size */}
+          <p className="text-lg md:text-xl my-4 text-black">{subtitle}</p> {/* Adjusted font size */}
           <button className="flex-none rounded-md bg-[#1f1f1f] px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-gray-800 transition-colors">
             <span className="px-1">{buttonText}</span>
           </button>
@@ -37,4 +36,3 @@ export default function Hero({ title, subtitle, buttonText, image }: HeroProps) 
     </section>
   )
 }
-
