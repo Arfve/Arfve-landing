060b9a95561518adfbafa52648e3f891880ed350
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2025-03-05 17:09:14 +0100

    crowdfunding update

--- src/app/crowdfounding/page.tsx
+++ src/app/crowdfounding/page.tsx
@@ -2,12 +2,18 @@ import BrandStory  from "@/components/crowdfunding/brandStory";
 import { getCrowdfoundingPage } from "@/lib/getCrowdfoundingData";
 import IndustryEndorsements from "@/components/crowdfunding/industryEndorsements";
 import SignUpSection from "@/components/crowdfunding/signUpSection";
+import DummyBuild from "@/components/crowdfunding/dummyBuild";
 export default async function page() {
   const {brandStory, industryEndorsements,signUpSection } = await getCrowdfoundingPage()
 
   return (
     <main>
       <BrandStory {...brandStory} />
+      <SignUpSection {...signUpSection} />
+      <DummyBuild/>
+      <DummyBuild/>
+      <DummyBuild/>
+      <DummyBuild/>
       <SignUpSection {...signUpSection} />
       <IndustryEndorsements {...industryEndorsements} />
      
