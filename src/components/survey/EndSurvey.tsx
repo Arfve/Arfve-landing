227c86a9fa6802751b68977150fb13863000f35c
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2025-03-12 16:13:56 +0100

    crowdfunding, survey, removed the local database, added responsive for phones, Updated product so user can change product by color

--- src/components/survey/EndSurvey.tsx
+++ src/components/survey/EndSurvey.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function EndSurvey() {
   return (
     <div className="fixed top-0 left-0 flex justify-center w-[100vw] h-[100vh] bg-white z-1 bg-white/30 backdrop-blur-sm ">
-      <div className="fixed p-9 top-1/4 flex flex-col gap-1 bg-blue-500 border w-[80vw] h-[500px] rounded-lg">
+      <div className="fixed p-9 top-1/4 flex flex-col gap-1 bg-white border w-[40vw] min-h-[400px] rounded-lg border-solid border-3 border-sky-500">
         <p>
           <strong>Thank You for Completing the Survey!</strong>
         </p>
